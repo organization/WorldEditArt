70847fb2191f5facabb49feb15fa0d1432163aa5
PEMapModder <pemapmodder1970@gmail.com>
2015-10-24 11:46:20 +0800

    Added build mechanism

---
+++ make.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * WorldEditArt
+ *
+ * Copyright (C) 2015 PEMapModder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PEMapModder
+ */
+
+use pemapmodder\worldeditart\WorldEditArt;
+
+chdir(__DIR__);
+
+$filename = "bin/WorldEditArt_Dev.phar";
+$class = "dev";
+$opts = getopt("", ["rc", "beta"]);
+if(isset($opts["beta"])){
+	$filename = "bin/WorldEditArt_Beta.phar";
+	$class = "beta";
+}
+if(isset($opts["rc"])){
+	$filename = "bin/WorldEditArt_RC.phar";
+	$class = "rc";
+}
+
+$data = json_decode(file_get_contents("bin/id.json"));
+$nextBuildId = ++$data->{$class};
+$globalBuildId = ++$data->global;
+$author = $data->author;
+$version = $data->version->stage . "_" . $data->version->major . "." . $data->version->minor . "." . $globalBuildId . "_" . $class . "#" . $nextBuildId;
+$description = $data->description;
+$website = $data->website;
+file_put_contents("bin/id.json", json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_BIGINT_AS_STRING));
+
+if(is_file($filename)){
+	unlink($filename);
+}
+
+$pluginYml = yaml_emit($manifest = [
+	"name" => "WorldEditArt",
+	"authors" => [
+		"PEMapModder",
+	],
+	"version" => $version,
+	"api" => ["1.13.0"], // TODO 2.0.0 support
+	"main" => WorldEditArt::class,
+	"description" => $description,
+	"website" => $website,
+	"build" => [
+		"author" => $author,
+		"buildTime" => [
+			"ISO8601" => date(DATE_ISO8601),
+			"unix" => time(),
+		],
+		"type" => $class,
+		"globalBuildNumber" => $globalBuildId,
+		"typeVersion" => $nextBuildId
+	]
+], YAML_UTF8_ENCODING, YAML_LN_BREAK);
+
+$i = 0;
+function addDir(\Phar $phar, $realPath, $localPath){
+	global $i;
+	$realPath = str_replace("\\", "/", $realPath);
+	$localPath = rtrim($localPath, "/\\") . "/";
+	echo "Directory transfer: $realPath > $localPath", PHP_EOL;
+	foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($realPath)) as $path){
+		if(!$path->isFile()){
+			continue;
+		}
+		$path = str_replace("\\", "/", (string) $path);
+		if(strpos($path, ".git") !== false){
+			continue;
+		}
+		$relative = ltrim(substr($path, strlen($realPath)), "/");
+		$local = $localPath . $relative;
+		$num = str_pad((string) (++$i), 3, "0", STR_PAD_LEFT);
+		echo "\r[$num] Adding: " . realpath($path) . " to $local";
+		$phar->addFile($path, $local);
+	}
+	echo "\n";
+}
+
+$phar = new Phar($filename);
+$phar->setStub('<?php require_once "entry/entry.php"; __HALT_COMPILER();');
+$phar->setSignatureAlgorithm(Phar::SHA1);
+$phar->setMetadata($manifest);
+$phar->startBuffering();
+$phar->addFromString("plugin.yml", $pluginYml);
+addDir($phar, realpath("src"), "src");
+addDir($phar, realpath("resources"), "resources");
+$phar->stopBuffering();
--- src/pemapmodder/worldeditart/WorldEditArt.php
+++ src/pemapmodder/worldeditart/WorldEditArt.php
@@ -15,10 +15,31 @@ namespace pemapmodder\worldeditart;
  * @author PEMapModder
  */
 
+use pocketmine\permission\Permission;
 use pocketmine\plugin\PluginBase;
 
 class WorldEditArt extends PluginBase{
 	public function onEnable(){
 		$this->saveDefaultConfig();
+		$fh = $this->getResource("permissions.json");
+		$json = stream_get_contents($fh);
+		fclose($fh);
+		$perms = json_decode($json, true);
+		$stack = [];
+		$this->walkPerms($stack, $perms);
+		var_dump($perms);
+		Permission::loadPermissions($perms);
+	}
+	private function walkPerms(array $stack, array &$perms){
+		$prefix = implode(".", $stack) . ".";
+		foreach(array_keys($perms) as $key){
+			$perms[$prefix . $key] = $perms[$key];
+			unset($perms[$key]);
+			$stack2 = $stack;
+			$stack2[] = $key;
+			if(isset($perms[$prefix . $key]["children"])){
+				$this->walkPerms($stack2, $perms[$prefix . $key]["children"]);
+			}
+		}
 	}
 }
