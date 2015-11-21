bd7d86c0b3dc9a899ebd5bad035f33dd7d1f7fe6
PEMapModder <pemapmodder1970@gmail.com>
2015-11-21 14:08:04 +0800

    Updated BufferedIterator

--- doxygen/d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.js
+++ doxygen/d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.js
@@ -4,6 +4,7 @@ var classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace =
     [ "getRadius", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#a46ed26029924e916f771089ac7c3291f", null ],
     [ "getTopCenter", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#afda7dd81aef914d85b0be4b00642b831", null ],
     [ "isInside", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#acf949cf74dbd51f1b04de8df58d3f0a5", null ],
+    [ "iteratorAllBlocks", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#a68332c79e76916b07258cfaece3f14f6", null ],
     [ "serialize", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#a4d9813fea8525b19a1d370eeb8fe41d6", null ],
     [ "setBaseCenter", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#a192bd8e9c5fa791809393a05fe42bfff", null ],
     [ "setRadius", "d2/d9b/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1CylinderSpace.html#a8bc49e5c1c1e748b8255b7c3ed3b7d1f", null ],
--- doxygen/dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.js
+++ doxygen/dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.js
@@ -4,6 +4,7 @@ var classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace =
     [ "getRadius", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#a46ed26029924e916f771089ac7c3291f", null ],
     [ "isInside", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#acf949cf74dbd51f1b04de8df58d3f0a5", null ],
     [ "isValid", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#a7b37efab7473a1effc29f8be2421f6e3", null ],
+    [ "iteratorAllBlocks", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#a68332c79e76916b07258cfaece3f14f6", null ],
     [ "serialize", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#a4d9813fea8525b19a1d370eeb8fe41d6", null ],
     [ "setRadius", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#a8bc49e5c1c1e748b8255b7c3ed3b7d1f", null ],
     [ "unserialize", "dd/d74/classpemapmodder_1_1worldeditart_1_1libworldedit_1_1space_1_1SphereSpace.html#ac2f3a0997c46fd9bb24fe4190f738eb0", null ]
